6411dc0f8021272bceb53236093566915a80eaf3
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-01 23:27:19 +0300

    module.css

--- basics/src/components/03-styles/04-external-module-styling.js
+++ basics/src/components/03-styles/04-external-module-styling.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './04-external.module.css'
 
 export default function ExternalModuleStyling() {
+    const {externalModuleStyling,title} = styles; // destructuring
   return (
-    <div>ExternalModuleStyling</div>
+    <div className={externalModuleStyling}>
+        <h2 className={title}>React Hooks</h2>
+        <p>zcjklcjxklzjasdasdasdsckljczkxczlkxcjklc</p>
+    </div>
   )
 }
