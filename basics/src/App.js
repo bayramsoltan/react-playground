dcf8acd2cf6c46ef4cfd77ed6ec3bfd743cf98cb
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-01 22:34:48 +0300

    inline-styling

--- basics/src/App.js
+++ basics/src/App.js
@@ -4,6 +4,7 @@ import Jsx1 from './components/2-jsx/01-jsx';
 import Jsx2 from './components/2-jsx/02-jsx';
 import Jsx3 from './components/2-jsx/03-jsx';
 import Jsx4 from './components/2-jsx/04-jsx';
+import InlineStyling from './components/03-styles/01-inline-styling';
 
 //import etmeyi unutmu bu klasor tum companentleri uzerinde bir html sayfasi gibi
 export default function App() {
@@ -13,9 +14,15 @@ export default function App() {
             <Bananas1 />
             <Orange />
             <Jsx1> </Jsx1>
+            <br/>       
             <Jsx2></Jsx2>
+            <br/>
             <Jsx3></Jsx3>
+            <br/>
             <Jsx4/>
+            <br/>
+            <InlineStyling/>
+            <br/>
         </>
     )
 };
