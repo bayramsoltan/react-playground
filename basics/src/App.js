16428712b6b4f7b10179435f923ecbda47b93890
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-02 22:48:19 +0300

    digital-time

--- basics/src/App.js
+++ basics/src/App.js
@@ -31,6 +31,8 @@ export default function App() {
             <ExternalStyling/>
             <br/>
             <ExternalModuleStyling/>
+            <br/>
+            <DijitalSaatUygulamasi1/>
         </>
     )
 };
