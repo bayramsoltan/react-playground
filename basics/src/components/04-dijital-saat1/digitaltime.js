787c9c9353a9bf460d986bddaa9f32c7ef5eca55
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-03 19:37:03 +0300

    first-react-app

--- basics/src/components/04-dijital-saat1/digitaltime.js
+++ basics/src/components/04-dijital-saat1/digitaltime.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import "./digitaltime1.scss";
+import moment from 'moment';
 
 const Digitaltime1 = () => {
+    const dateTime = moment();
+    const timeStr = dateTime.format("HH:mm");
+    const dateStr = dateTime.format("LL");
+    const dayStr = moment().format("dddd");
+    const hour = moment().format("HH");
+    //moment().hour() yada .hours() kullanilabilir
+
+    let message = '';
+
+    if(hour >= 7 &&  hour <11){
+        message = "MORNING"
+    }else if(hour>= 11 && hour <14){
+        message = "NOON"
+    }else if(hour>17 && hour < 22){
+        message = "EVENING"
+    }else{
+        message = "NIGHT"
+    }
+
   return (
-    <div><h2>Digital</h2></div>
+    <div className='dijital-time-app'>
+        <div className='time'>{timeStr}</div>
+        <div>
+            <div className='date'>{dateStr}</div>
+            <div className='day'>{dayStr} {message}</div>
+        </div>
+    </div>
   )
 }
 
