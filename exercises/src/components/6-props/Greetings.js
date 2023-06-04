7c12dc4f209234be81b64f167131860e0d44af0d
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-05 00:49:35 +0300

    props-exercises

---
+++ exercises/src/components/6-props/Greetings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greetings = (props) => {
+  return (
+    <div className='greetings'>
+        <h1>Welcome {props}</h1>
+    </div>
+  )
+}
+
+export default Greetings
