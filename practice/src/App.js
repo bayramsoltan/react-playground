e4df9aaa0a20821f2c888ecd809fa9293e949ae9
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-16 19:20:06 +0300

    fetch mockapi

--- practice/src/App.js
+++ practice/src/App.js
@@ -4,17 +4,21 @@ import Cards from './c9/StudentCard'
 import StudentCard from './c9/StudentCard'
 import Header from './c10/Header'
 import Alisveris from './hw/Alisveris'
+import Sairler from './c12/Sairler'
+import Product from './c17/product'
 
 const App = () => {
   return (
     <div>
       {/* <Kisi/> <Header/>*/}
-      <Alisveris/>
+      {/* <Alisveris/>
       <Rating
       style={{ maxWidth: 180 }}
       value={3}
       readOnly
-    />
+    /> */}
+    {/* <Sairler/> */}
+    <Product/>
     </div>
   )
 }
