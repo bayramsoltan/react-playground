411e8a74d7c4a975f86959e657ba1bbf5431d71b
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-16 22:00:43 +0300

    counterwith_two_way_data_bindin

--- basics/src/components/23-lifting-state-up/02-childComponent.js
+++ basics/src/components/23-lifting-state-up/02-childComponent.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+const ChildComponent = (props) => {
 
-const ChildComponent = () => {
-  return (
-    <div>
-        <h2>CIFT YONLU VERI AKTARIMI </h2>
-        <Button>
+    // const increaseCounter = () => {
+    //     props.handleCounter(1)
+    // }
 
-        </Button>
-    </div>
-  )
+    const increaseCounter = () => {
+        props.setCounter(prev => prev + 1)
+    }
+    console.log('ChildComponent rendered')
+    // TODO: dogrudan setter'i ver onclick'e
+
+    return (
+        <div>
+            <h2>ÇİFT YÖNLÜ VERİ AKTARIMI</h2>
+            <Button
+                variant='outline-success'
+                onClick={increaseCounter}
+            >
+                Arttir
+            </Button>
+        </div>
+    )
 }
 
 export default ChildComponent
