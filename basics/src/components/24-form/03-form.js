7ddcb93967fa7f1b8255307586589bab8c3eceb3
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-06-17 21:19:55 +0300

    axios-forms

---
+++ basics/src/components/24-form/03-form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Form3 = () => {
+  return (
+    <Container>
+        <h2>Form-Pratik</h2>
+    </Container>
+  )
+}
+
+export default Form3
