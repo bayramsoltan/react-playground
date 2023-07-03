edb503d5c579bd5f6f9dac09866418fee89352f0
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-07-03 21:23:40 +0300

    context-api-example

--- context/simple/src/App.js
+++ context/simple/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+    const [currencies, setCurrencies] = useState();
+
+    const loadData = async()=>{
+        try{
+            const response = await
+            fetch(`${settings.API_URL}/latest?
+            from=${settings.currency.default}`)
+        }
+        catch (error){
+            console.log(error)
+        }
+    }
   return (
     <div>App</div>
   )
