dda716c2a4e210e37068e2129192a958f5f802d6
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-07-11 00:56:02 +0300

    counter bug is found/ ()=>handleClick(0)

--- exercises/src/components/16-counter/counter.js
+++ exercises/src/components/16-counter/counter.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react'
-import { Button, ButtonGroup } from 'react-bootstrap';
-import {AiOutlineMinusCircle, AiOutlinePlusCircle} from "react-icons/ai"
-import {GrPowerReset} from "react-icons/gr";
+import React, { useState } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
+import { GrPowerReset } from "react-icons/gr";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
 
-const [counter, setCounter] = useState(0);
+  const handleClick = (val) => {
+    if (val < 0) val = 0;
+    setCounter(val);
+  };
 
-const handleClick =(val) =>{
-    if(val<0) val=0;
-    setCounter(val)
-}
+  return (
+    <ButtonGroup aria-label="Basic example">
+      <Button variant="info" onClick={() => handleClick(counter + 1)}>
+        <AiOutlinePlusCircle />
+      </Button>
+      <Button variant="light" disabled>
+        {counter}
+      </Button>
+      <Button variant="warning" onClick={() => handleClick(counter - 1)}>
+        <AiOutlineMinusCircle />
+      </Button>
+      <Button variant="danger" onClick={()=>handleClick(0)}>
+        <GrPowerReset />
+      </Button>
+    </ButtonGroup>
+  );
+};
 
-    return (
-        <ButtonGroup aria-label="Basic example">
-          <Button variant="info" onClick={()=>handleClick(counter+1)}><AiOutlinePlusCircle/></Button>
-          <Button variant="light" disabled>{counter}</Button>
-          <Button variant="warning" onClick={()=>handleClick(counter - 1)}><AiOutlineMinusCircle/></Button>
-          <Button variant="danger" onClick={handleClick(0)}><GrPowerReset/></Button>
-        </ButtonGroup>
-      );
-}
-
-export default Counter
+export default Counter;
