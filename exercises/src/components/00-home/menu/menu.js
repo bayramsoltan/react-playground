6a050739faab3148d07b929989d7712f6e87924c
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-07-20 23:18:02 +0300

    react router dom exercises all

--- exercises/src/components/00-home/menu/menu.js
+++ exercises/src/components/00-home/menu/menu.js
@@ -11,6 +11,53 @@ const Menu = () => {
             <li><Link to="/jsx3">Jsx3</Link></li>
             <li><Link to="/jsx4">Jsx4</Link></li>
             <li><Link to="/jsx5">Jsx5</Link></li>
+
+            <li><Link to="/jsx6">Jsx6</Link></li>
+            <li><Link to="/style1">Style1</Link></li>
+            <li><Link to="/style2">Style2</Link></li>
+            <li><Link to="/style3">Style3</Link></li>
+            <li><Link to="/style4">Style4</Link></li>
+            <li><Link to="/clock1">Clock1</Link></li>
+            <li><Link to="/greetings">Greetings</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/clock2">Clock2</Link></li>
+            <li><Link to="/image">Image</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/profilecard">ProfileCard</Link></li>
+
+            <li><Link to="/bootstrapstatic">BootstrapStatic</Link></li>
+            <li><Link to="/bootstrapdynamic">BootstrapDynamic</Link></li>
+            <li><Link to="/icon">Icon</Link></li>
+            <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/jsx7">Jsx7</Link></li>
+
+            <li><Link to="/stateless">Stateless</Link></li>
+            <li><Link to="/state">State</Link></li>
+            <li><Link to="/counter">Counter</Link></li>
+            <li><Link to="/birthday">Birthday</Link></li>
+
+            <li><Link to="/progressbarexample">ProgressBarExample</Link></li>
+            <li><Link to="/useeffect1">UseEffect1</Link></li>
+            <li><Link to="/clock3">Clock3</Link></li>
+
+            <li><Link to="/useffect2">UseEffect2</Link></li>
+            <li><Link to="/countryfilter">CountryFilter</Link></li>
+            <li><Link to="/useref">UseRef</Link></li>
+
+            <li><Link to="/scroll">Scroll</Link></li>
+            <li><Link to="/classcomp">ClassComp</Link></li>
+            <li><Link to="/usercards">userCards</Link></li>
+            <li><Link to="/countries">Countries</Link></li>
+            <li><Link to="/parentcomp">ParentComp</Link></li>
+            <li><Link to="/form1">Form1</Link></li>
+            <li><Link to="/form2">Form2</Link></li>
+            <li><Link to="/form3">Form3</Link></li>
+            <li><Link to="/form4">Form4</Link></li>
+            <li><Link to="/form5">Form5</Link></li>
+            <li><Link to="/form6">Form6</Link></li>
+
+            
+
         </ul>
     </nav>
   )
