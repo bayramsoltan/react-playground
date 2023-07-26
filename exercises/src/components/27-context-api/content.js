94989538dcc1bb288252ae00e1eee9761ef2cf38
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-07-26 19:38:29 +0300

    money exchanger

--- exercises/src/components/27-context-api/content.js
+++ exercises/src/components/27-context-api/content.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
+import StoreContext from '../../store';
 
 const Content = () => {
+    const [amount, setAmount] = useState(1);
+    const [currency, setCurrency] = useState("USD");
+
+    const [result, setResult] = useState(0);
+
+    const store = useContext(StoreContext);
+    const {currencies} = store;
+
+
+useEffect(() => {
+  const rslt = (amount / currencies[currency]).toFixed(2);
+  setResult(rslt);
+}, [amount, currency])
+
+
   return (
-    <div>Content</div>
+    <div>
+        <InputGroup className="mb-3">
+        <Form.Control value={amount} onChange={(e)=>setAmount(e.target.value)}/>
+        <Form.Select value={currency} onChange={(e)=>setCurrency(e.target.value)}>
+            <option value="1">USD</option>
+            <option value="2">EUR</option>
+        </Form.Select>
+        <InputGroup.Text id="basic-addon1">Result: {result}</InputGroup.Text>
+      </InputGroup>
+    </div>
   )
 }
 
