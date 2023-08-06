2f00bcbaab9d04ba72605700c1f5a4dcb7a314ce
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-08-07 00:01:10 +0300

    login-logout

--- auth/src/pages/home-page.js
+++ auth/src/pages/home-page.js
@@ -2,16 +2,33 @@ import React from 'react'
 import { Button, ButtonGroup } from 'react-bootstrap'
 import { useStore } from '../store'
 import { counterDown, counterUp } from '../store/counter/couter-actions';
+import { Link } from 'react-router-dom';
+import { logout } from '../store/auth/auth-actions';
 
 const HomePage = () => {
 
-    const {counterState, dispatchCounter} = useStore();
-    console.log(counterState);
+    const {counterState, dispatchCounter, authState, dispatchAuth} = useStore();
+    const {counter} = counterState;
+    const {isUserLogin, user} = authState;
+
+
+    const handleLogout = () =>{
+      const result = window.confirm("Are you sure to logout?");
+      if(!result) return;
+      dispatchAuth(logout());
+    }
+
   return (
     <div>
+      <h1>Hello {user.firstName} {user.lastName}</h1>
+      <h2> {isUserLogin ? 
+      (<Button variant="danger" onClick={handleLogout}>Logout</Button>) :
+        ( <Button variant="primary" as={Link} to="/login">Login</Button>)
+      }
+      </h2>
     <ButtonGroup aria-label="Basic example">
       <Button variant="warning" onClick={()=>dispatchCounter(counterDown())}>-</Button>
-      <Button variant="secondary" disabled>{counterState.counter}</Button>
+      <Button variant="secondary" disabled>{counter}</Button>
       <Button variant="info" onClick={()=>dispatchCounter(counterUp())}>+</Button>
     </ButtonGroup>
     </div>
--- auth/src/pages/login-page.js
+++ auth/src/pages/login-page.js
@@ -2,55 +2,48 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useStore } from "../store";
-import { loginSuccess } from "../store/auth/auth-actions";
+import { loginFail, loginSuccess } from "../store/auth/auth-actions";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE_URL = "https://carrental-v3-backend.herokuapp.com";
-
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  const { dispatchAuth } = useStore();
 
-  const {dispatchAuth} = useStore();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const payload = {
+      email,
+      password,
+    };
 
-const handleLogin = async(e) =>{
-  e.preventDefault();
-  const payload = {
-    email,password
+    try {
+      setLoading(true);
+
+      const respAuth = await axios.post(`${API_BASE_URL}/login`, payload);
+      const token = respAuth.data.token;
+
+      const authHeader = { Authorization: `Bearer ${token}` };
+      const respUser = await axios.get(`${API_BASE_URL}/user`, {
+        headers: authHeader,
+      });
+
+      // dispatchAuth({type: "LOGIN_SUCCESS",payload:respUser.data});
+      dispatchAuth(loginSuccess(respUser.data));
+      // console.log(respUser.data);
+      navigate("/");
+    } catch (err) {
+      alert(err.response.data.message);
+      dispatchAuth(loginFail());
+    } finally {
+      setLoading(false);
+    }
   };
-
-try {
-  setLoading(true);
-
-  const respAuth = await axios.post(`${API_BASE_URL}/login`,payload);
-  const token = respAuth.data.token;
-
-  const authHeader = {Authorization:`Bearer ${token}`};
-  const respUser = await axios.get(`${API_BASE_URL}/user`,{
-    headers:authHeader,
-  });
-
-// dispatchAuth({type: "LOGIN_SUCCESS",payload:respUser.data});
-dispatchAuth(loginSuccess(respUser.data));
-  // console.log(respUser.data);
-  navigate("/");
-
-} catch (err) {
-  alert(err.response.data.message);
-
-}
-finally{
-  setLoading(false);
-}
-
-
-
-}
-
 
   return (
     <Container>
@@ -71,8 +64,7 @@ finally{
             type="password"
             value={password}
             required={true}
-            onChange={(e) => setPassword(e.target.value)
-          }
+            onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
 
